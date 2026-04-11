4ab80a91d6ad6ade5b10666f42b601a478ca14dd
vaghinakpetrosian <147193790+vaghinakpetrosian@users.noreply.github.com>
2026-04-11 04:44:19 +0400

    Refactor ApiManager class and update methods

--- apimanager.h
+++ apimanager.h
@@ -3,34 +3,30 @@
 
 #include <QObject>
 #include <QNetworkAccessManager>
-#include <QNetworkReply>
 #include <QJsonArray>
-#include <QJsonDocument>
-#include <QJsonObject>
 
 class ApiManager : public QObject {
     Q_OBJECT
-
 public:
     explicit ApiManager(QObject *parent = nullptr);
+    void loginUser(const QString &username);
+    void search(const QString &query);
     void fetchRecommendations(int userId);
     void fetchLikedSongs(int userId);
     void fetchArtists(int userId);
+    void fetchFollowedArtists(int userId);
     void sendLike(int userId, int trackId);
     void sendFollow(int userId, int artistId);
-    void fetchFollowedArtists(int userId);
-
-    void loginUser(const QString &username);
-    void search(const QString &query);
+    void fetchArtistTracks(int artistId);
 
 signals:
+    void userLoggedIn(int userId, const QString &username);
+    void searchResultsReady(const QJsonArray &results);
     void recsReady(const QJsonArray &tracks);
     void likesReady(const QJsonArray &tracks);
     void artistsReady(const QJsonArray &artists);
     void followedArtistsReady(const QJsonArray &artists);
-    void userLoggedIn(int userId, const QString &username);
-    void searchResultsReady(const QJsonArray &tracks);
-    void errorOccurred(const QString &error);
+    void artistTracksReady(const QJsonArray &tracks);
 
 private:
     QNetworkAccessManager *networkManager;
